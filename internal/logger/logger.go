8f9ea907c2db2356e26be2c2bcdec30edfa94f91
Anton Naumov <anton.naumov@iss.ru>
2026-08-10 19:27:13 +0300

    Iteration 6 (2)

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,35 +1,13 @@
 package logger
 
 import (
-	"errors"
 	"fmt"
-	"sync"
 
 	"go.uber.org/zap"
 )
 
-var (
-	LoggerAlreadyInitialized = errors.New("logger already initialized")
-	LoggerNotInitialized     = errors.New("logger not initialized")
-)
-
-type loggersStorage struct {
-	LoggersMap map[string]*zap.Logger
-	Mu         sync.Mutex
-}
-
-var loggers loggersStorage = loggersStorage{
-	LoggersMap: make(map[string]*zap.Logger),
-}
-
 // initialize logger with loggerName
 func InitializeRootLogger(loggerName string, level string) (*zap.Logger, error) {
-	loggers.Mu.Lock()
-	if _, ok := loggers.LoggersMap[loggerName]; ok {
-		return nil, LoggerAlreadyInitialized
-	}
-	loggers.Mu.Unlock()
-
 	lvl, err := zap.ParseAtomicLevel(level)
 
 	if err != nil {
@@ -46,23 +24,5 @@ func InitializeRootLogger(loggerName string, level string) (*zap.Logger, error) 
 		return nil, err
 	}
 
-	loggers.Mu.Lock()
-	defer loggers.Mu.Unlock()
-
-	loggers.LoggersMap[loggerName] = zl
-
 	return zl, nil
-}
-
-func GetRootLogger(loggerName string) (*zap.Logger, error) {
-	loggers.Mu.Lock()
-	defer loggers.Mu.Unlock()
-
-	logger, ok := loggers.LoggersMap[loggerName]
-
-	if !ok {
-		return nil, LoggerNotInitialized
-	}
-
-	return logger, nil
 }
--- internal/server/url_shortener_server.go
+++ internal/server/url_shortener_server.go
@@ -1,23 +1,24 @@
 package server
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/apnaumov/url-shortener.git/internal/config"
 	"github.com/apnaumov/url-shortener.git/internal/handler"
+	"github.com/apnaumov/url-shortener.git/internal/logger"
 )
 
 func StartUrlShortenerServer() {
+	logger, err := logger.InitializeRootLogger("server", "info")
 	conf := config.InitConfig()
 
 	router, err := handler.NewUrlShortenerRouter(conf.ServerBaseUrl)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err.Error())
 	}
 
 	err = http.ListenAndServe(conf.ServerListenAddr, router.Mux)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err.Error())
 	}
 }

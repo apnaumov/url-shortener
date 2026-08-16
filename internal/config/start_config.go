4d299b5ddac89fa7647f4bb08ca11488cd676776
Anton Naumov <anton.naumov@iss.ru>
2026-08-16 13:17:07 +0300

    Iteration 9 (2)

--- internal/config/start_config.go
+++ internal/config/start_config.go
@@ -14,36 +14,33 @@ type Config struct {
 func InitConfig() Config {
 	config := Config{}
 
+	setConfigFromArgs(&config)
+	setConfigFromEnv(&config)
+
+	return config
+}
+
+func setConfigFromArgs(config *Config) {
 	const defaultListen = ":8080"
 	const defaultUrl = "http://localhost:8080"
 	const defaultFileStoragePath = "url_storage.storage"
 
-	var needToParseArgs bool
+	flag.StringVar(&config.ServerListenAddr, "a", defaultListen, `Address to run server. Default: ":8080"`)
+	flag.StringVar(&config.ServerBaseUrl, "b", defaultUrl, `Base address of the resulting shortened URL. Default: "http://localhost:8080"`)
+	flag.StringVar(&config.FileStoragePath, "f", defaultFileStoragePath, `File path to storage file. Default: "$(pwd)/url_storage.storage"`)
+	flag.Parse()
+}
 
+func setConfigFromEnv(config *Config) {
 	if listenAddr, found := os.LookupEnv("SERVER_ADDRESS"); found {
 		config.ServerListenAddr = listenAddr
-	} else {
-		flag.StringVar(&config.ServerListenAddr, "a", defaultListen, `Address to run server. Default: ":8080"`)
-		needToParseArgs = true
 	}
 
 	if urlAddr, found := os.LookupEnv("BASE_URL"); found {
 		config.ServerBaseUrl = urlAddr
-	} else {
-		flag.StringVar(&config.ServerBaseUrl, "b", defaultUrl, `Base address of the resulting shortened URL. Default: "http://localhost:8080"`)
-		needToParseArgs = true
 	}
 
 	if storageFilePath, found := os.LookupEnv("FILE_STORAGE_PATH"); found {
 		config.FileStoragePath = storageFilePath
-	} else {
-		flag.StringVar(&config.FileStoragePath, "f", defaultFileStoragePath, `File path to storage file. Default: "$(pwd)/url_storage.storage"`)
-		needToParseArgs = true
 	}
-
-	if needToParseArgs {
-		flag.Parse()
-	}
-
-	return config
 }

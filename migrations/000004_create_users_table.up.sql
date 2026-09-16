bb3effbc0fb35abd5daeb0f134aa701f0f3112e5
Anton Naumov <anton.naumov@iss.ru>
2026-09-16 18:03:56 +0300

    It

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS shortener_url_users (
 	user_id SERIAL NOT NULL PRIMARY KEY
 );
 
@@ -7,6 +7,6 @@ ADD COLUMN IF NOT EXISTS user_id INTEGER;
 
 ALTER TABLE shortener_urls
 ADD CONSTRAINT fk_shortener_urls_user
-FOREIGN KEY (user_id) REFERENCES users(user_id)
+FOREIGN KEY (user_id) REFERENCES shortener_url_users(user_id)
 ON DELETE CASCADE 
 ON UPDATE CASCADE;
